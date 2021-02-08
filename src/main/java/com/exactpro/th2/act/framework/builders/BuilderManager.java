d2f60c8613137d47234e76f560837af6bfa8ee20
Alexey Karpukhin <7701579+battlemaster@users.noreply.github.com>
2021-02-08 20:34:00 +0300

    [TH2-1005] (#9)
    [TH2-1005] Updated according new th2-hand actions

--- src/main/java/com/exactpro/th2/act/framework/builders/BuilderManager.java
+++ src/main/java/com/exactpro/th2/act/framework/builders/BuilderManager.java
@@ -95,6 +95,10 @@ public class BuilderManager {
 		return new WaitForElementBuilder(context);
 	}
 
+	public MaximizeMainWindowBuilder maximizeMainWindow() {
+		return new MaximizeMainWindowBuilder(context);
+	}
+
 	public UIFrameworkContext getContext() {
 		return context;
 	}
